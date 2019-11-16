fe3dd44914f937aee867d3a66ad797dcd1ad6ffe
ntando <ntandocyrilnkuna@gmail.com>
2019-11-16 20:16:25 +0200

    connect to local  database

---
+++ server/Model/index.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+//set debug
+mongoose.set('debug', true);
+//set mongoose to allow promises
+mongoose.Promise = Promise;
+//connect to database
+mongoose.connect(process.env.DATABASE_LOCAL, {
+  keepAlive: true,
+  useNewUrlParser: true,
+  useFindAndModify: false // find and modify is deprecated
+});
+
+module.exports.User = require('./User');
+module.exports.Product = require('./Product');
+module.exports.Review = require('./Review');
+module.exports.Order = require('./Order');
