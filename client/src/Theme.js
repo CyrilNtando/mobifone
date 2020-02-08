9324a499b63340f8072cab5a447cd34370c8c6b1
ntando <ntandocyrilnkuna@gmail.com>
2020-02-08 17:40:13 +0200

    added Global Styles and Global Themes styles

---
+++ client/src/Theme.js
@@ -0,0 +1,21 @@
+const Theme = {
+  colorPrimary: '#eb2f64',
+  colorPrimaryLight: '#ff3366',
+  colorPrimaryDark: '#ba265d',
+  //Grey colors
+  colorGreyLight1: '#faf9f9',
+  colorGreyLight2: '#f4f2f2',
+  colorGreyLight3: '#f0eeee',
+  colorGreyLight4: '#ccc',
+  // dark grey colors
+  colorGreyDark1: '#333',
+  colorGreyDark2: '#777',
+  colorGreyDark3: '#999',
+  //spacing and sizes
+  gridWidth: '114rem',
+  gridGutterHorizontal: '6rem',
+  gridGutterVertical: '8rem',
+  line: '1px solid #cacaca'
+};
+
+export default Theme;
